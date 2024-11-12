15c8e299d77b1dc11c8342b13a12922153c65e27
manimaduri <manimaduri2@gmail.com>
2024-11-12 16:20:10 +0530

    init components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <h1>Welcome to {{ title }}!</h1>
+    <app-home [title]="title" />
     <router-outlet />
   `,
   styles: [],
